4877ce37f62adb16249ed43312a0739824724c50
Art2m Lozovenko <tema77@yahoo.com>
2024-09-22 22:11:34 +0300

    init commit

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
